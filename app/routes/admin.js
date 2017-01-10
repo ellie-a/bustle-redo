06b921b54164e20dcd9d8dc7a8600f30517a8716
Chris Clifford and Ellie Angell <chris@>
2017-01-10 13:22:47 -0800

    add admin form

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions : {
     saveArticle(params) {
+      console.log(params);
       var newArticle = this.store.createRecord('article', params);
       newArticle.save();
       this.transitionTo('home');
